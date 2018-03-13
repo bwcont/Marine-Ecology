7295ad59c6ec2d4cd07355809d14c1755bdf6a91
E.C <35582713+bwcont@users.noreply.github.com>
2018-03-12 23:37:31 -0700

    Update (2) - Figures Code LT50 and Kaplan-Meier.R

--- AirVWater/(2) - Figures Code LT50 and Kaplan-Meier.R
+++ AirVWater/(2) - Figures Code LT50 and Kaplan-Meier.R
@@ -86,7 +86,7 @@ WTWC<-subset(LT50Data, LT50Data$name == 'Water.TP.week.C')
 WTTA<-subset(LT50Data, LT50Data$name == 'Water.TP.triweek.A')
 WTTB<-subset(LT50Data, LT50Data$name == 'Water.TP.triweek.B')
 WTTC<-subset(LT50Data, LT50Data$name == 'Water.TP.triweek.C')
-#####
+###Loop for LT50s...####
 
 small<-list(ASBA, ASBB, ASBC, ASDA, ASDB, ASDC, ASWA, ASWB, ASWC, ASTA, ASTB, ASTC, ATBA, ATBB, ATBC, ATDA, ATDB, ATDC, ATWA, ATWB, ATWC, ATTA, ATTB, ATTC, WSBA, WSBB, WSBC, WSDA, WSDB, WSDC, WSWA, WSWB, WSWC, WSTA, WSTB, WSTC, WTBA, WTBB, WTBC, WTDA, WTDB, WTDC, WTWA, WTWB, WTWC, WTTA, WTTB, WTTC)
 unique(small)
@@ -117,20 +117,23 @@ results[,1]<-c('ASBA', 'ASBB', 'ASBC', "ASDA", 'ASDB', 'ASDC', 'ASWA', 'ASWB', '
 results[,5]<-c('ASB', 'ASB', 'ASB', "ASD", 'ASD', 'ASD', 'ASW', 'ASW', 'ASW', 'AST', 'AST', 'AST', 'ATB', 'ATB', 'ATB', 'ATD', 'ATD', 'ATD', 'ATW', 'ATW', 'ATW', 'ATT', 'ATT', 'ATT', 'WSB', 'WSB', 'WSB', 'WSD', 'WSD', 'WSD', 'WSW', 'WSW', 'WSW', 'WST', 'WST', 'WST', 'WTB', 'WTB', 'WTB', 'WTD', 'WTD', 'WTD', 'WTW', 'WTW', 'WTW', 'WTT', 'WTT', 'WTT')
 
 
-#JellyBean All (n=16)...####
+###JellyBean All...####
+#Get a data summary to extract your plus and minuses
 data.summary<-ddply(results, c('gen.name'), summarize,
                     mean.lt50 = mean(LT50, na.rm = TRUE), 
                     N = sum(!is.na(LT50)),
                     se.lt50 = sd(LT50, na.rm = TRUE)/sqrt(N))
 
+#Get the plus and minus of your points
 data.summary$se.plus<-(data.summary$mean.lt50 + data.summary$se.lt50)
-
 data.summary$se.minus<-(data.summary$mean.lt50 - data.summary$se.lt50)
+
 
 data.summary
 data.summary2<-data.summary[1:16,]
 data.summary2<-data.summary2[c(2,4,1,3,6,8,5,7,10,12,9,11,14,16,13,15),]
 
+#plot jellybean
 plot(0,type='n', #make empty plot 
      xlim=c(0.75,16.25) # x limits
      , ylim=c(24, 40) # y limits 
@@ -156,11 +159,74 @@ points(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16), data.summary2$mean.lt50, cex =
 axis(2, cex.axis=1.2, tick = FALSE)
 box()
 
+###JellyBean Time clumped...########
+data.summary<-ddply(results, c('gen.name'), summarize,
+                    mean.lt50 = mean(LT50, na.rm = TRUE), 
+                    N = sum(!is.na(LT50)),
+                    se.lt50 = sd(LT50, na.rm = TRUE)/sqrt(N))
+
+#Get the plus and minus of your points
+data.summary$se.plus<-(data.summary$mean.lt50 + data.summary$se.lt50)
+data.summary$se.minus<-(data.summary$mean.lt50 - data.summary$se.lt50)
+
+
+data.summary
+data.summary2<-data.summary[1:16,]
+data.summary2<-data.summary2[c(2,4,1,3,6,8,5,7,10,12,9,11,14,16,13,15),]
+
+#plot jellybean
+plot(0,type='n', #make empty plot 
+     xlim=c(0.75,16.25) # x limits
+     , ylim=c(24, 40) # y limits 
+     , xlab= '', # name x azis
+     ylab = 'LT50', # name y axis
+     main = 'All LT50s', # main title (on middle of plot)
+     pch=19, yaxt='n', axes=F, # removed axes
+     cex.lab=1.05) # make empty plot to fill in 
+arrows(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16), data.summary2$se.plus, # bottom of arrows
+       c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16), data.summary2$se.minus, # top of arrows
+       angle = 90, code = 3, length = 0, lty = 1, lwd = 30, # jelly bean width
+       col = palette(rainbow(16, s = 0.9, v = 1)))
+axis(1, at=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16), labels= data.summary2$gen.name, cex.axis=1.2, # text size
+     las = 1, tick = FALSE) 
+points(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16), data.summary2$mean.lt50, cex = 2, pch = 16, col = 'black')
+axis(2, cex.axis=1.2, tick = FALSE)
+box()
 
 
 
 
-#########
+
+
+
+
+
+
+
+
+
+
+#####################
+####
+###
+##
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
