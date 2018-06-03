684064800d31d94c3c8b670b7b254b37869d9519
E.C <35582713+bwcont@users.noreply.github.com>
2018-06-03 03:54:04 -0700

    Update Graphs_Micro_Age.R

--- LifeStageModel/Graphs_Micro_Age.R
+++ LifeStageModel/Graphs_Micro_Age.R
@@ -1,6 +1,6 @@
 ################################################# 
 # Title: Microhabitat & Size Class for M. californianus
-# Purpose: Survey Model comparison
+# Purpose: Graphs for Microhabitat and Size Class Work
 # Created by : E. Cruz & L. Pandori
 # Created on : 5/21/2018
 # Last Edited : 6/3/2018
