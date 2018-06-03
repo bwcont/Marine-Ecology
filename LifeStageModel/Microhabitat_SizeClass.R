146fd155a5dc194391c5b8b7b0dffa673d035f02
E.C <35582713+bwcont@users.noreply.github.com>
2018-06-03 02:29:28 -0700

    Update Microhabitat_SizeClass.R

--- LifeStageModel/Microhabitat_SizeClass.R
+++ LifeStageModel/Microhabitat_SizeClass.R
@@ -50,21 +50,46 @@ arrows(c(1,2,3,4,5,7,8,9,10,11), c(-0.95-0.08, -0.67-0.53 , -0.90-0.21, -0.62-0.
        angle = 90, code = 3, length = 0, lty = 1, lwd = 12, # jelly bean width
        col = Col.MicroHab)
 
-points(x = c(1,2,3,4,5,7,8,9,10,11), y = c(-0.95, -0.67 , -0.90,-0.62,-0.87,-0.97,-0.73,-0.92,-0.72 ,-0.76), cex = 1.5, pch = 16 )
+points(x = c(1,2,3,4,5,7,8,9,10,11), y = c(-0.95, -0.67 , -0.90,-0.62,-0.87,-0.97,-0.73,-0.92,-0.72 ,-0.76), cex = 1.4, pch = 16 )
 
 axis(1, c(3, 9), c("LCDM", 'CCSP'), cex=2, pch=16, col = "black")
 axis(2)
 abline(h=0)
 legend("topright", legend=c('UnShelt_Solitary',
-                        'UnShelt_Aggregate',
-                        'Sheltered_Solitary',
-                        'Sheltered_Aggregate',
-                        'Tidepool'), fill = Col.MicroHab, cex=0.8)
+                            'UnShelt_Aggregate',
+                            'Sheltered_Solitary',
+                            'Sheltered_Aggregate',
+                            'Tidepool'), fill = Col.MicroHab, cex=0.9)
 
 box()
 
 
-###Fig. 3: TBd####
+###Fig. 3: Size Class Distribution Association With Microhabitat####
+plot(0,type='n', #make empty plot 
+     xlim=c(0.75,11.25) # x limits
+     , ylim=c(-1.15,1) # y limits 
+     , xlab= ' ', # name x azis
+     ylab = 'Association', # name y axis
+     main = ' ', # main title (on middle of plot)
+     pch=19, yaxt='n', axes=F, # removed axes
+     cex.lab=1.05) # make empty plot to fill in
 
+arrows(c(1,2,3,4,5,7,8,9,10,11), c(-0.95-0.08, -0.67-0.53 , -0.90-0.21, -0.62-0.43, -0.87-0.18, -0.97-0.07, -0.73-0.28, -0.92-0.19, -0.72-0.32 ,-0.76-0.38), 
+       c(1,2,3,4,5,7,8,9,10,11), c(-0.95+0.08, -0.67+0.53 , -0.90+0.21, -0.62+0.43, -0.87+0.18, -0.97+0.07, -0.73+0.28, -0.92+0.19, -0.72+0.32 ,-0.76+0.38),
+       angle = 90, code = 3, length = 0, lty = 1, lwd = 12, # jelly bean width
+       col = Col.MicroHab)
+
+points(x = c(1,2,3,4,5,7,8,9,10,11), y = c(-0.95, -0.67 , -0.90,-0.62,-0.87,-0.97,-0.73,-0.92,-0.72 ,-0.76), cex = 1.4, pch = 16 )
+
+axis(1, c(3, 9), c("LCDM", 'CCSP'), cex=2, pch=16, col = "black")
+axis(2)
+abline(h=0)
+legend("topright", legend=c('UnShelt_Solitary',
+                            'UnShelt_Aggregate',
+                            'Sheltered_Solitary',
+                            'Sheltered_Aggregate',
+                            'Tidepool'), fill = Col.MicroHab, cex=0.9)
+
+box()
 
 
