dee3b770508373e7b0b5135006d37591f45a8a95
E.C <35582713+bwcont@users.noreply.github.com>
2018-06-03 01:34:40 -0700

    Update Microhabitat_SizeClass.R

--- LifeStageModel/Microhabitat_SizeClass.R
+++ LifeStageModel/Microhabitat_SizeClass.R
@@ -37,22 +37,30 @@ Col.MicroHab <- c('#FF0000','#FF8000', '#00CC00', '#66B2FF', '#B266FF')
 
 ###Fig. 2: Average Association of Micro per site####
 plot(0,type='n', #make empty plot 
-     xlim=c(0.75,3.25) # x limits
-     , ylim=c(0, 27) # y limits 
-     , xlab= 'Tide Height', # name x azis
-     ylab = 'Average Total Mussels', # name y axis
+     xlim=c(0.75,11.25) # x limits
+     , ylim=c(-1,1) # y limits 
+     , xlab= ' ', # name x azis
+     ylab = 'Association', # name y axis
      main = ' ', # main title (on middle of plot)
      pch=19, yaxt='n', axes=F, # removed axes
      cex.lab=1.05) # make empty plot to fill in
 
-arrows(c(1,2,3), c(4.9 + 10.78, 7.5 + 18.58, 0.9 + 2.102), 
-       c(1,2,3), c(4.9 - 10.78, 7.5 - 18.58, 0.9 - 2.102),
+arrows(c(1,2,3,4,5,7,8,9,10,11), c(4.9 + 10.78, 7.5 + 18.58, 0.9 + 2.102), 
+       c(1,2,3,4,5,7,8,9,10,11), c(4.9 - 10.78, 7.5 - 18.58, 0.9 - 2.102),
        angle = 90, code = 3, length = 0, lty = 1, lwd = 5, # jelly bean width
        col = Col.MicroHab)
 
-points(x = c(1,2,3), y = c(4.9, 7.5, 0.91), cex = 1.5, pch = 16 )
+points(x = c(1,2,3,4,5,7,8,9,10,11), y = c(-0.95, -0.67 , -0.90,-0.62,-0.87,-0.97,-0.73,-0.92,-0.72 ,-0.76), cex = 1.5, pch = 16 )
 
-axis(1, c(1,2,3), c("Low", "Mid", "High"), cex=2, pch=16, col = "black")
+axis(1, c(3, 8), c("LCDM", 'CCSP'), cex=2, pch=16, col = "black")
 axis(2)
 
 box()
+
+
+
+
+###Fig. 3: TBd####
+
+
+
